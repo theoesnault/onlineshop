fab48f32874bde9f8d31525b51bbf7f0851a27c0
Théo Esnault <theo.esnault@gmail.com>
2020-08-29 18:10:13 +0200

    Angular - HomePage + angular material ...

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AccountComponent } from './components/account/account.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'favorites', component: FavoritesComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+import { MaterialModule } from './material-modules';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AccountComponent } from './components/account/account.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HomepageComponent,
+    CategoriesComponent,
+    SidenavComponent,
+    AccountComponent,
+    FavoritesComponent,
+    MessagesComponent,
+    SettingsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
